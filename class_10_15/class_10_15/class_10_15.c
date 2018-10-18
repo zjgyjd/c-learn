7806ddbd42ce78ccfa0bf20c7a515d29889f7e15
zjgyjd <632032465@qq.com>
2018-10-18 12:29:37 +0800

    提交

--- class_10_15/class_10_15/class_10_15.c
+++ class_10_15/class_10_15/class_10_15.c
@@ -7,6 +7,8 @@
 #define Chess 5//表示三子棋
 //1.定义一个全局变量数组
 char g_chesses[ROW][COL];//初始化
+int Computer[ROW][COL][4];//代表横竖左斜右斜
+int Player[ROW][COL][4];//存储棋型找最优解,按照棋型的重要程度给出一个分
 void reset();
 void Prtboard();
 void PlayerMove();
@@ -23,6 +25,12 @@ int Isline(int row,int col);
 int IsCol(int row, int col);
 int IsAng1(int row, int col);
 int IsAng2(int row, int col);
+//void ChessAnalysis(int row, int col);
+//int CLine(int row, int col,char flag);
+//int CCOL(int row, int col,char flag);
+//int CAng1(int row, int col,char flag);
+//int CAngR(int row, int col,char flag);
+//int Qimap(int count);
 
 int main(){
 	//重置
@@ -51,6 +59,14 @@ void reset( ){//重置
 	for (int row = 0; row < ROW; row++){
 		for (int col = 0; col < ROW; col++){
 			g_chesses[row][col] = ' ';
+		}
+	}
+	for (int row = 0; row < ROW; row++){
+		for (int col = 0; col < ROW; col++){
+			for (int look = 0; look < 4; look++){
+				Computer[row][col][look] = 0;//look代表横竖左斜右斜
+				Player[row][col][look] = 0;//初始化
+			}
 		}
 	}
 }
@@ -107,7 +123,70 @@ void PlayerMove(){//1.利用坐标进行移动
 	}
 	g_chesses[row][col] = 'X';//玩家为X
 }
+//void ChessAnalysis(int row, int col){
+//	//判断是否为空处
+//	if (g_chesses[row][col] != ' '){
+//		return;
+//	}//向左和右判断是否为'O'即电脑下的并判断分数
+//	Computer[row][col][0] = CLine(row,col,'O');//表示该空位的行
+//	Computer[row][col][1] = CCOL(row,col,'O');//表示该空位的列
+//	Computer[row][col][2] = CAng1(row, col,'O');//表示向左斜
+//	Computer[row][col][3] = CAngR(row, col,'O');//表示向右斜
+//}
+//int CAngl(int row, int col, char flag){
+//	int uprow = row - 1;
+//	int lcol = col - 1;
+//}
+//int CCOL(int row,int col,char flag){
+//	int com_Upcount = 0;
+//	int uprow = row - 1;
+//	while (uprow >= 0 && g_chesses[uprow][col] == flag){
+//		com_Upcount++;
+//		uprow = row - 1;
+//	}
+//	int dowrow = row + 1;
+//	while (dowrow < ROW&&g_chesses[dowrow][col] == flag){
+//		com_Upcount++;
+//		uprow = row + 1;
+//	}
+//	return Qimap(com_Upcount++);
+//}
+//int CLine(int row,int col,char flag){
+//	int com_Lcount = 0;
+//	int lcol = col - 1;
+//	while (lcol >= 0&&g_chesses[row][lcol] == flag){//判断是否达到边界
+//		com_Lcount++;
+//		lcol = lcol - 1;
+//	}
+//	int rcol = col + 1;
+//	while (rcol < COL&&g_chesses[row][rcol] == flag){
+//		com_Lcount++;
+//		lcol = lcol + 1;
+//	}
+//	return Qimap(com_Lcount++);
+//}
 void ComputerMove(){
+	//int row = 0;//从左上角第一个开始查找
+	//int col = 0;
+	////for (; row < ROW; row++){
+	//	for (; col < COL; col++){
+	//		ChessAnalysis(row, col);//查看分析棋型
+	//	}
+	//}
+	//int max = Computer[0][0][0];
+	//int Row = 0; 
+	//int Col = 0;
+	//for (row = 0; row < ROW; row++){//2.遍历三维数组
+	//	for (col = 0; col < COL; col++){//查找最高分
+	//		for (int look = 0; look < 4; look++){
+	//			if (max < Computer[row][col][look]){
+	//				max = Computer[row][col][look];
+	//				Row = row;
+	//				Col = col;
+	//			}
+	//		}
+	//	}
+	//}
 	int row = rand() % (ROW );//0到下标最大值
 	int col = rand() % (COL );
 	while (g_chesses[row][col] != ' '){
